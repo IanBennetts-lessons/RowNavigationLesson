ea67f8ae01d19a42517201ae1afeb2ee5d89ea15
Ian Bennetts <ian.bennetts@softgineering.com>
2020-07-14 08:30:04 +1000

    Initial Commit

---
+++ RowLession/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RowLession
+//
+//  Created by Ian Bennetts on 14/7/20.
+//  Copyright © 2020 Ian Bennetts. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
